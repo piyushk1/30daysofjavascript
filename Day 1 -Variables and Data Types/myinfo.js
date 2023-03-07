879fb153bfa3c8e49355621598a9d8c843789ea1
Piyush kukade <piyushkukade1@gmail.com>
2023-03-08 00:07:15 +0530

    Day 1

---
+++ Day 1 -Variables and Data Types/myinfo.js
@@ -0,0 +1,12 @@
+// Create variables to store your name, age, and favorite color.
+//  Use the console.log() function to display the values of your variables in the console.
+
+
+let fname = "Piyush";
+let lname = "Kukade";
+let age = 23;
+let favorite_color = "Black";
+
+console.log(fname + " " +  lname);
+console.log(age);
+console.log(favorite_color);
